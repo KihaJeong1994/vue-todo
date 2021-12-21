0fd7bb98b54b597205b504c618fa96abddf6e64d
KihaJeong1994 <jkha7371@gmail.com>
2021-12-21 17:50:20 +0900

    component 구조 짜기

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader/>
+    <TodoFooter/>
+    <TodoList/>
+    <TodoInput/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoHeader from './components/Header/TodoHeader.vue'
+import TodoFooter from './components/Footer/TodoFooter.vue'
+import TodoList from './components/List/TodoList.vue'
+import TodoInput from './components/Input/TodoInput.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    "TodoHeader":TodoHeader,
+    "TodoFooter":TodoFooter,
+    "TodoList":TodoList,
+    "TodoInput":TodoInput
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
